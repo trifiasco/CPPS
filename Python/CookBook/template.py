4b6c228d450d004049c42582cebd9befc60a66c0
Arnab Roy <trifiasco@gmail.com>
2021-03-04 03:53:22 +0600

    update python template file

--- Python/CookBook/template.py
+++ Python/CookBook/template.py
@@ -2,15 +2,22 @@ import sys
 import os
 import math
 
+LOCAL_ENV: bool = os.environ.get('USER') == 'trifiasco'
+
 
 def READ(fileName):
-    script_dir = os.path.dirname(__file__)
-    username = "trifiasco"
-    if username in script_dir:
-        sys.stdin = open(script_dir + '/' + fileName, 'r')
+    sys.stdin = open(
+        '{path}/{fileName}'.format(path=os.path.dirname(__file__), fileName=fileName), 'r')
 
 
 if __name__ == "__main__":
-    # READ('in.txt')
-    print('this is a test')
+    if LOCAL_ENV:
+        READ('in.txt')
+
+    while True:
+        try:
+            n = int(input())
+            # do stuff
+        except EOFError:
+            break
     pass
