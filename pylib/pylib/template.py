f0e4e4ecbb90c9fc4774fef36ef9d76642fe7fc4
Arnab Roy <trifiasco@gmail.com>
2025-01-25 13:33:28 +0100

    initial

---
+++ pylib/pylib/template.py
@@ -0,0 +1,23 @@
+import sys
+import os
+
+LOCAL_ENV: bool = os.environ.get('USER') == 'trifiasco'
+
+
+def SET_LOCAL_READ():
+    sys.stdin = open('debug/in.txt', 'r') # input file
+    #sys.stdout = open('out.txt', 'w')
+
+if __name__ == "__main__":
+    sys.setrecursionlimit(200000)
+    if LOCAL_ENV:
+        SET_LOCAL_READ()
+
+    while True:
+        try:
+            n = int(input())
+            print(n)
+            # do stuff
+        except EOFError:
+            break
+    pass
